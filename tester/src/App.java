ffa49a56a6270887216e503999271f91b3e90c76
Oliver Egholm Folkersen <oliver@MacBook-Pro-tilhrende-Oliver-2.local>
2025-10-06 09:49:04 +0200

    Din commit-besked her

--- tester/src/App.java
+++ tester/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Din mor er tyk");
+        System.out.println("Virker det?");
+        System.out.println("jigfiji");
     }
 }
